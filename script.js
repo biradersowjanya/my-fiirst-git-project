e20ead875e2748a93abb500771e45ea83c2c25c0
Birader Sowjanya <biradersowjanya@gmail.com>
2025-08-28 16:41:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const yourName = "sowjanya"; 
-const now = new Date();
-console.log(`${sowjanya} - ${now.toLocaleString()}`);
+function sayHello() {
+    alert("Hello from JavaScript! 🚀");
+    console.log("Button clicked! This is logged in the console.");
+    console.log("Button clicked! This is logged in the console 222.");
+    console.log("Hello from feature-hello branch");
+}
+
+// Some example utility functions
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+
+// Testing the functions in console
+console.log(capitalize("hello world")); // Hello world
+console.log(sumArray([1, 2, 3, 4])); // 10
